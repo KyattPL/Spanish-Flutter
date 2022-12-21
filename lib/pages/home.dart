8997a66079fba6b90f4ab7ab711e885027112b24
Kajetan Pynka <siema1234@gazeta.pl>
2022-12-21 02:24:37 +0100

    Made home page

---
+++ lib/pages/home.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Spanish-Flutter'),
+        centerTitle: true,
+        backgroundColor: Colors.brown[800],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20.0),
+        color: Colors.brown[900],
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: RawMaterialButton(
+                      onPressed: () {},
+                      fillColor: Colors.brown[700],
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 32),
+                            child: Row(
+                              children: [
+                                Expanded(child: Image.asset('assets/english.png')),
+                                const Expanded(child: Icon(Icons.arrow_right, size: 72,)),
+                                Expanded(child: Image.asset('assets/spanish.png'))
+                              ],
+                            ),
+                          ),
+                          Text('ENG -> ESP', style: TextStyle(color: Colors.brown[200], fontSize: 64))
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20,),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: RawMaterialButton(
+                      onPressed: () {},
+                      fillColor: Colors.brown[700],
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 32),
+                            child: Row(
+                              children: [
+                                Expanded(child: Image.asset('assets/spanish.png')),
+                                const Expanded(child: Icon(Icons.arrow_right, size: 72,)),
+                                Expanded(child: Image.asset('assets/english.png'))
+                              ],
+                            ),
+                          ),
+                          Text('ESP -> ENG', style: TextStyle(color: Colors.brown[200], fontSize: 64))
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
